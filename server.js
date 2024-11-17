b9ad129555f71c8497c4a668d39e880b5f61eef0
yonas790 <yoni2752@gmail.com>
2024-11-17 11:23:15 +0300

    final commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from './app/app.js';
 import http from 'http'
 
 const server = http.createServer(app);
-const PORT = 5000;
-server.listen(PORT, ()=>{
+
+server.listen(process.env.PORT, ()=>{
     console.log(`server is running at ${PORT}`);
 });
